cd75fd8a9b17683248e8721673580de6a2f4859a
Shashikanth Reddy <kolanshashii@gmail.com>
2022-02-14 11:55:13 +0530

    added sorting for transactions

--- src/store/TransactionsContext.js
+++ src/store/TransactionsContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, orderBy, onSnapshot, query } from 'firebase/firestore';
 
 import { db } from '../services/firebase-config';
 import { AuthContext } from './AuthContext';
@@ -18,37 +18,35 @@ export const TransactionsProvider = (props) => {
   const authCtx = useContext(AuthContext);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(
-      collection(db, authCtx.user.uid),
-      (snapshot) => {
-        const incomeItemsList = [];
-        const expensesItemsList = [];
+    const q = query(collection(db, authCtx.user.uid), orderBy('date', 'desc'));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const incomeItemsList = [];
+      const expensesItemsList = [];
 
-        snapshot.docs.forEach((doc) => {
-          if (doc.data().amount > 0) {
-            incomeItemsList.push({
-              id: doc.id,
-              amount: doc.data().amount,
-              category: doc.data().category,
-              date: doc.data().date.toDate().toDateString(),
-              note: doc.data().note,
-            });
-          } else {
-            expensesItemsList.push({
-              id: doc.id,
-              amount: doc.data().amount,
-              category: doc.data().category,
-              date: doc.data().date.toDate().toDateString(),
-              note: doc.data().note,
-            });
-          }
-        });
+      snapshot.docs.forEach((doc) => {
+        if (doc.data().amount > 0) {
+          incomeItemsList.push({
+            id: doc.id,
+            amount: doc.data().amount,
+            category: doc.data().category,
+            date: doc.data().date.toDate().toDateString(),
+            note: doc.data().note,
+          });
+        } else {
+          expensesItemsList.push({
+            id: doc.id,
+            amount: doc.data().amount,
+            category: doc.data().category,
+            date: doc.data().date.toDate().toDateString(),
+            note: doc.data().note,
+          });
+        }
+      });
 
-        setIncomeItems(incomeItemsList);
-        setExpensesItems(expensesItemsList);
-        setIsLoading(false);
-      }
-    );
+      setIncomeItems(incomeItemsList);
+      setExpensesItems(expensesItemsList);
+      setIsLoading(false);
+    });
 
     return unsubscribe;
   }, [authCtx.user.uid]);
